1bb716a4974c45a7a13a9fd03f653776a53aa7c8
AishaOthman <aishoabukaseem@gmail.com>
2023-05-23 12:36:17 +0300

    adding Interfaces

--- packages/friable/src/Interfaces.ts
+++ packages/friable/src/Interfaces.ts
@@ -1,4 +1,21 @@
 export interface IRecipe {
-    taskName: string;
-    deadline: number;
+    recipeName: string;
+    utherName: string;
+    prepTime:number;
+    cookTime:number;
+    category:string;
+    diteType:string;
+    ratings:number;
+    skilLevel:string;
+    dishType:string;
+    serves:number;
+    ingredients:string[];
+    instructions:string[];
+  }
+
+  export interface IUser {
+    userName:string;
+    userEmail:string;
+    userPassword:string;
+
   }
--- packages/friable/src/pages/Home/Home.tsx
+++ packages/friable/src/pages/Home/Home.tsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 
 const Home = ()=>{
-    return (<div>
+    return (
+    <div>
       <div>Hello</div>
       <Link to="/"> to results</Link>
       <Link to="/Login"> to Login</Link>
