93735f3951aea6f9ae54543243ef0f0ff5b3dd1e
AishaOthman <aishoabukaseem@gmail.com>
2023-06-15 18:43:58 +0300

    editing log in component

--- packages/friable/src/components/RecipeIngRow.tsx
+++ packages/friable/src/components/RecipeIngRow.tsx
@@ -1,9 +1,60 @@
-import React, { useContext } from "react";
-
+import { Typography, Grid, InputLabel, Select, MenuItem, TextField, Button, Autocomplete } from "@mui/material";
+import React, { useContext, useEffect, useState } from "react";
+import { IRecipe, ingredient } from "shared_data";
+import { RecipeContext } from "./addNewRecipeForm";
+import axios from "axios";
 
 const RecipeIngRow = () => {
+  
+  const {recipe ,setRecipe} = useContext(RecipeContext);
+
+  const [ingredients,setIngredients] = React.useState<ingredient[]>([])
+ 
+  const [recipes,setRecipes] = React.useState<IRecipe[]>([]);
+
+  useEffect(()=>{
+    async function getNeo4jIngredients() {
+      try{
+        const serverIngredientsResponse = await axios.get('/neo4j/ingredients');
+        setIngredients(serverIngredientsResponse.data)
+      }catch(err){
+        console.log(err)
+      }
+    }
+    getNeo4jIngredients();
+  },[])
+  
+ 
+
+ 
   return (
-    <div>RecipeIngRow</div>
+    <div> 
+      <Typography variant='h5' component='h5' marginTop={2}>
+         Add Igredients:
+       </Typography>
+         <Grid item xs={12}   >
+         <Autocomplete
+          
+          id="combo-box-demo"
+          options={ingredients.map(i=>i.name)}
+        
+          sx={{ width: 300 }}
+          renderInput={(params:any) => <TextField {...params} label="ingredients" />}
+        />
+        
+       
+        <TextField label="amount"  />
+       <div>
+       <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          //onClick={ AddNewIngredient}
+        >
+         add another ingredient 
+        </Button>
+       </div>
+        </Grid></div>
   )
 }
 
